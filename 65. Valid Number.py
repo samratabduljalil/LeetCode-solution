cb3903c4788ed1f543ed2f9b0622ffa68c743c19
Samrat Abdul Jalil <95965521+samratabduljalil@users.noreply.github.com>
2024-10-25 19:54:20 +0600

    65. Valid Number (solved)

--- 65. Valid Number.py
+++ 65. Valid Number.py
@@ -6,41 +6,56 @@ class Solution:
         count_e = 0
         count_d =0
         count_c=0
+        flagd=0
         for i in range(0,len(s)):
             if s[i]>='0' and s[i]<='9':
                 flagnum=1
                 if(flag==1):
                     flag=0
             elif (s[i]>='A' and s[i]<='Z') or (s[i]>='a' and s[i]<='z') :
-                if s[i]>='e' or s[i]<='E':
+                if s[i]=='e' or s[i]=='E':
                     if flagnum==1 and count_e==0:
-                        
                         flag=1
                         count_e +=1
-                        print(count_e)
                     else :
                         return False
-                    
+                else:
+                    return False    
             elif s[i]=='+' or s[i]=='-':
-                
-                if (flag==1 or flagnum==1) and flagmp<=2:
-                    flag=0
+                if flagd==1:
+                   return False
+                if count_d>1:
+                    return False   
+                elif (flag==1 and flagnum==1) and flagmp<=2:
+                    flag=1   
+                elif flagnum==1 :
+                    return False
                 elif flagmp==0:
                        flagmap=1
                 else:
-                    return False
-
-            
-               
+                    return False  
                 flagmp=+1
+                flag=1
             elif s[i]=='.':
-                
-                
-                if count_d ==0 and flagnum==1 and count_e==0 :
-                    cout_d =+1
+                flagd=1
+                if flagnum==0:
+                    flag=1
+                if count_d ==0 and  count_e==0 :
+                    count_d +=1
+                    continue
                 else:
                     return False
-        if flag==0:
+                count_d +=1
+                continue
+            flagd=0
+        print(flag)
+        if flag==0 and (count_d!=len(s)):
             return True 
         else :
-            return False         
+            return False           
+
+
+
+                
+
+        
