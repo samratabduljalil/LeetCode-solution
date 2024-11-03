89248b0b9691e9b9d4e2a6651e4694ffabed3011
Samrat Abdul Jalil <95965521+samratabduljalil@users.noreply.github.com>
2024-11-03 13:21:49 +0600

    add string

--- 415. Add Strings.py
+++ 415. Add Strings.py
@@ -1,18 +1,18 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        Max_lenth_num=0
-        Min_lenth_num=0
+        Max_length_num=0
+        Min_length_num=0
         Digit_after_addition=[]
         On_Hand=0
         if len(num1)>len(num2):
-            Max_lenth_num=num1
-            Min_lenth_num=num2
+            Max_length_num=num1
+            Min_length_num=num2
         else:
-            Max_lenth_num=num2
-            Min_lenth_num=num1
-        for i in range(len(Min_lenth_num)):
-            digit1 = ord(Min_lenth_num[len(Min_lenth_num)-1-i])-ord('0')
-            digit2 = ord(Max_lenth_num[len(Max_lenth_num)-1-i])-ord('0')
+            Max_length_num=num2
+            Min_length_num=num1
+        for i in range(len(Min_length_num)):
+            digit1 = ord(Min_length_num[len(Min_length_num)-1-i])-ord('0')
+            digit2 = ord(Max_length_num[len(Max_length_num)-1-i])-ord('0')
             result=digit1+digit2+On_Hand
             On_Hand=0
             if result <10:
@@ -22,8 +22,8 @@ class Solution:
                 retrive_digit=result%10
                 Digit_after_addition.append(retrive_digit)
                 On_Hand=1
-        for i in range(len(Min_lenth_num),len(Max_lenth_num)):
-            digit1 = ord(Max_lenth_num[len(Max_lenth_num)-1-i])-ord('0')
+        for i in range(len(Min_length_num),len(Max_length_num)):
+            digit1 = ord(Max_length_num[len(Max_length_num)-1-i])-ord('0')
             if On_Hand==1:
                 result=digit1+On_Hand
                 if result <10:
